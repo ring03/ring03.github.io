3fcb18123c2074ba79a4929e39cfc7e44d772099
ring03 <checl3@hotmail.com>
2015-11-25 11:07:53 +0800

    project echo

--- js/script.js
+++ js/script.js
@@ -237,9 +237,40 @@ function showPreloader() {
 }
 
 function hidePreloader() {
-  $(".preloader").delay(2000).fadeOut("slow");
+  $(".preloader").delay(1000).fadeOut("slow");
 }
 
+/*-------------------------------------------
+Back to Top
+---------------------------------------------*/
 
+function backTop() {
+$(document).ready(function(){
+ 
+    // hide #back-top first
+    $("#back-top").hide();
+  
+  // fade in #back-top
+  $(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $('#back-top').fadeIn();
+      } else {
+        $('#back-top').fadeOut();
+      }
+    });
+ 
+    // scroll body to 0px on click
+    $('#back-top a').click(function () {
+      $('body,html').animate({
+        scrollTop: 0
+      }, 800);
+      return false;
+    });
+  });
+ 
+});
+
+}
 
 })//End
